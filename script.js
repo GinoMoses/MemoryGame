d9e8aa90bc4c56ef64346f3f9efc6dfa32a5e0ae
Damian Gorol <damian.gorol78@gmail.com>
2023-02-19 14:31:34 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,46 @@
+let points = 0;
+let choiceCount = 0;
+let chosenCards = [];
+let boardActive = true;
+
+const choose = async index => {
+    const card = document.querySelector(`[data-index="${index}"]`);
+    if(card.dataset.status == "active" && boardActive == true) {
+
+        card.style.backgroundColor = card.dataset.c;
+        choiceCount++;
+        chosenCards[choiceCount-1] = card;
+        if(choiceCount==2) {
+            boardActive = false;
+            checkIfCorrect();
+            checkWin();
+        }
+    }
+}
+
+const checkIfCorrect = () => {
+    if(chosenCards[0].dataset.c == chosenCards[1].dataset.c) {
+        points++;
+        chosenCards.forEach((element) => {
+            element.dataset.status = "inactive";
+        })
+        boardActive = true;
+    } else {
+        setTimeout(() => {
+            chosenCards.forEach((element) => {
+                element.style.backgroundColor = "#222";  
+            })
+            boardActive = true;
+        }, 400);
+    }
+    choiceCount = 0;
+}
+
+const checkWin = () => {
+    if(points == 12) {
+        setTimeout(() => {
+            alert('Gratulacje');
+            location.reload();
+        }, 200);
+    }
+} 
